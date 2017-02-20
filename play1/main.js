6c577b4a87c022480ff5d4d9c22448d6276623b4
biosckon <biosckon@hotmail.com>
2017-02-20 22:04:07 +0100

    change tabs to 4

--- play1/main.js
+++ play1/main.js
@@ -3,35 +3,31 @@ var p = body.append('p');
 p.text('new paragraph');
 
 d3.csv('food.csv', (error, data) => {
-  if (error) {
-    console.log(error);
-  } else {
-    dataset = data;
-    body.selectAll('div')
-      .data(dataset)
-      .enter()
-      .append('div')
-      .attr('class', 'bar')
-      .style('height', d => {
-        return (d.Deliciousness * 5) + 'px';
-      });
-  }
+    if (error) {
+        console.log(error);
+    } else {
+        dataset = data;
+        body.selectAll('div')
+        .data(dataset)
+        .enter()
+        .append('div')
+        .attr('class', 'bar')
+        .style('height', d => (d.Deliciousness * 5) + 'px');
+    }
 });
 
 var ds2 = [];
 for (var i = 0; i < 25; i++) {
-  var newNum = Math.random() * 30;
-  ds2.push(newNum);
+    var newNum = Math.random() * 30;
+    ds2.push(newNum);
 }
 
 body.selectAll('div')
-  .data(ds2)
-  .enter()
-  .append('div')
-  .attr('class', 'bar2')
-  .style('height', d => {
-    return d + 'px';
-  });
+    .data(ds2)
+    .enter()
+    .append('div')
+    .attr('class', 'bar2')
+    .style('height', d => d + 'px');
 
 
 // SVG
@@ -41,28 +37,28 @@ var h = 50;
 var svg = body.append('svg');
 svg.attr('width', w).attr('height', h);
 var circles = svg.selectAll('circle')
-  .data(ds2)
-  .enter()
-  .append('circle');
+    .data(ds2)
+    .enter()
+    .append('circle');
 
 circles.attr('cx', (d, i) => (i * 50) + 25)
-  .attr('cy', h / 2)
-  .attr('r', d => d)
-  .attr('fill', 'yellow')
-  .attr('stroke', 'orange')
-  .attr('stroke-width', d => d/2);
+    .attr('cy', h / 2)
+    .attr('r', d => d)
+    .attr('fill', 'yellow')
+    .attr('stroke', 'orange')
+    .attr('stroke-width', d => d/2);
 
-  // bar chart with SVG
-  var barPadding = 1;
-  var svg2 = body.append('svg')
-    .attr('width', w).attr('height', h)
+// bar chart with SVG
+var barPadding = 1;
+var svg2 = body.append('svg')
+    .attr('width', w).attr('height', h * 4)
     .selectAll('rect').data(ds2).enter()
     .append('rect')
     .attr('x', (_, i) => i * (w / ds2.length))
-    .attr('y', d => h - d)
+    .attr('y', d => h - d * 4)
     .attr('width', w / ds2.length - barPadding)
-    .attr('height', d => d);
+    .attr('height', d => d * 4);
 
 
 
-  //
+//
