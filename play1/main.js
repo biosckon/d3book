1a298b29c7bd84e0e3cc845abd7d512d6a1e39a9
biosckon <biosckon@hotmail.com>
2017-02-20 18:52:36 +0100

    page 96 before making bar chart

--- play1/main.js
+++ play1/main.js
@@ -17,3 +17,43 @@ d3.csv('food.csv', (error, data) => {
       });
   }
 });
+
+var ds2 = [];
+for (var i = 0; i < 25; i++) {
+  var newNum = Math.random() * 30;
+  ds2.push(newNum);
+}
+
+body.selectAll('div')
+  .data(ds2)
+  .enter()
+  .append('div')
+  .attr('class', 'bar2')
+  .style('height', d => {
+    return d + 'px';
+  });
+
+
+// SVG
+var w = 500;
+var h = 50;
+
+var svg = body.append('svg');
+svg.attr('width', w).attr('height', h);
+var circles = svg.selectAll('circle')
+  .data(ds2)
+  .enter()
+  .append('circle');
+
+circles.attr('cx', (d, i) => {
+    return (i * 50) + 25;
+  })
+  .attr('cy', h / 2)
+  .attr('r', d => {
+    return d;
+  })
+  .attr('fill', 'yellow')
+  .attr('stroke', 'orange')
+  .attr('stroke-width', d => {
+    return d/2;
+  });
