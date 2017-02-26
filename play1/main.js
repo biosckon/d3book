e4405ffd6a6b64b4fdfaf8fa6f855efd39526a80
biosckon <biosckon@hotmail.com>
2017-02-26 16:56:52 +0100

    chapter 7 page 113

--- play1/main.js
+++ play1/main.js
@@ -51,12 +51,12 @@ circles.attr('cx', (d, i) => (i * 50) + 25)
 // bar chart with SVG
 var barPadding = 1;
 var svg2 = body.append('svg');
-svg2.attr('width', w).attr('height', h * 4)
+svg2.attr('width', w).attr('height', h * 2)
     .selectAll('rect').data(ds2).enter()
     .append('rect')
     .attr({
         x: (_, i) => i * (w / ds2.length),
-        y: d => (h - d) * 4,
+        y: d => h * 2 - d * 4,
         width: w / ds2.length - barPadding,
         height: d => d * 4,
         fill: d => `rgb(0, 0, ${Math.round(d * 10)})`
@@ -67,13 +67,46 @@ svg2.selectAll('text')
     .data(ds2).enter().append('text')
     .text(d => Math.round(d))
     .attr({
-        x: (_, i) => i * (w / ds2.length),
-        y: d => Math.round((h - d) * 4)
+        x: (_, i) => (i + 0.5) * (w / ds2.length),
+        y: d => Math.round(h * 2 - d * 4) + 14,
+        'font-family': 'sans-serif',
+        'font-size': '11px',
+        'fill': 'white',
+        'text-anchor': 'middle'
     });
 
 
+// ds3
+var ds3 = [];
+for (var i = 0; i < 25; i++) {
+    var newx = Math.random() * w;
+    var newy = Math.random() * h;
+    ds3.push([newx, newy]);
+}
 
+var svg3 = body.append('svg');
+svg3.attr({
+    width: w,
+    height: h
+});
+svg3.selectAll('circle')
+    .data(ds3).enter().append('circle')
+    .attr({
+        cx: d => d[0],
+        cy: d => d[1],
+        r: d => Math.sqrt(h - d[1])
+    })
 
-
+svg3.selectAll('text')
+    .data(ds3).enter().append('text')
+    .text(d => `${Math.round(d[0])}, ${Math.round(d[1])}`)
+    .attr({
+        x: d => Math.round(d[0]),
+        y: d => Math.round(d[1]),
+        'font-family': 'sans-serif',
+        'font-size': '11px',
+        'fill': 'red',
+        'text-anchor': 'middle'
+    });
 
 //
